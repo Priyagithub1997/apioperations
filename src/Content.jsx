a8e133f8e36f22e795bb33713673b6b73765c490
unknown <Priyanka>
2024-09-16 05:11:04 -0700

    add css

--- src/Content.jsx
+++ src/Content.jsx
@@ -10,7 +10,7 @@ const Content = ({ items, newitem, setnewitem, handlecheck, search, setsearch, h
           <input className='additem' value={newitem} onChange={(e) => setnewitem(e.target.value)} placeholder='Enter New Item'></input>
           <span><button onClick={() => handleadd(newitem)}>Add</button></span>
         </div>
-        <div>
+        <div className="searchdiv">
           <input className='search' type='text' value={search} onChange={(e) => setsearch(e.target.value)} placeholder='Search'></input>
         </div>
         </div>
